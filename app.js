da0895a6a1e4e34582032a1a0ed125ba6db7fe1d
Maria Vlasova <vlasova_ya@mail.ru>
2021-03-24 17:51:49 +0400

    creates todo

--- app.js
+++ app.js
@@ -16,12 +16,46 @@ const App = {
         return {
             placeholderString: 'Введите название заметки',
             title: 'Список заметок',
-            inputValue: ''
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
         }
     },
     methods: {
-        inputChangeHandler(event) {
-            this.inputValue = event.target.value
+        // inputChangeHandler(event) {
+        //     this.inputValue = event.target.value
+        // },
+        addNewNote() {
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue);
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        // inputKeyPress(event) {
+        //     console.log(event.key);
+        //     if (event.key === 'Enter') {
+        //         this.addNewNote();
+        //     }
+        // }
+        removeNote(index) {
+            console.log('removeNote', index);
+            this.notes.splice(index, 1);
+        }
+    },
+    computed: {
+        doubleCount() {
+            console.log('doubleCount')
+            return this.notes.length * 2;
+        },
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 10) {
+               this.inputValue = ''
+            }
+            console.log('input Value changed', value)
         }
     }
 }
